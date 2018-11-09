49219fab3343b36f0ceb2e11f9d284c982cbba0d
posew7 <posew7@gmail.com>
2018-11-09 21:00:27 +0300

    oop2

--- oop1.py
+++ oop1.py
@@ -15,7 +15,24 @@ class Calisan:
         self.maas = self.maas + self.maas*self.zam_oran
 
 calisan1 = Calisan("ali","veli",100)
+calisan2 = Calisan("deli","alim",200)
+calisan3 = Calisan("kerim","kemik",300)
+calisan4 = Calisan("kezban","demir",400)
 
 print(calisan1.nameSurnameYazdir())
 calisan1.zamYap()
 print("yeni maas : ",calisan1.maas," lira")
+
+listee = [calisan1, calisan2, calisan3, calisan4]
+
+maxmaas = -1
+index = -1
+
+for e in listee:
+    if (e.maas > maxmaas):
+        maxmaas = e.maas
+        index = e
+print()
+print("en yüksek maaşı alan kişi : ", e.nameSurnameYazdir())
+print(maxmaas," TL")
+
